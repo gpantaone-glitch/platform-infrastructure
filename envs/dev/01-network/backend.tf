c5873d76aa6b2cf2124351e89c80b7470ef3c766
gpantaone-glitch <gpantaone@gmail.com>
2026-03-06 00:15:47 +0530

    Added assume dev role in backends

--- envs/dev/01-network/backend.tf
+++ envs/dev/01-network/backend.tf
@@ -5,8 +5,8 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "tf-lock-org-platform"
     encrypt        = true
-#    assume_role = {
-#    role_arn       = "arn:aws:iam::078591672268:role/PlatformStateAccessRole"
-#    }
+    assume_role = {
+    role_arn = "arn:aws:iam::317976464242:role/terraform-deployer-role"
+    }
     }
 }
