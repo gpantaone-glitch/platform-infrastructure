d89d805f308384955bac94b04334152d23759d45
gpantaone-glitch <gpantaone@gmail.com>
2026-03-06 18:50:42 +0530

    created provider.tf

--- envs/dev/01-network/backend.tf
+++ envs/dev/01-network/backend.tf
@@ -5,8 +5,5 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "tf-lock-org-platform"
     encrypt        = true
-    assume_role = {
-    role_arn = "arn:aws:iam::317976464242:role/terraform-deployer-role"
-    }
     }
 }
