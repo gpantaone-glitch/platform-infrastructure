51e1b876e7fff7d31ffcdd21cb05c4aa31d066af
gpantaone-glitch <gpantaone@gmail.com>
2026-03-07 06:56:13 +0530

    Revert "created provider.tf"
    This reverts commit d89d805f308384955bac94b04334152d23759d45.
    
    Reverting the destroying part

--- envs/dev/01-network/backend.tf
+++ envs/dev/01-network/backend.tf
@@ -5,5 +5,8 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "tf-lock-org-platform"
     encrypt        = true
+    assume_role = {
+    role_arn = "arn:aws:iam::317976464242:role/terraform-deployer-role"
+    }
     }
 }
