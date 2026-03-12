a20a576d86b89b3efff0f9367c09b72c26f3cdf5
Gaurav Pantaone <gpantaone@gmail.com>
2026-03-12 21:54:56 +0530

    Create provider.tf

---
+++ envs/dev/05-gitops/provider.tf
@@ -0,0 +1,19 @@
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+
+  config = {
+    bucket = "tf-state-org-platform"
+    key    = "dev/02-eks/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
