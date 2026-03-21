126801f70cb71587a169e1086cd39f5d98074cf3
Gaurav Pantaone <gpantaone@gmail.com>
2026-03-21 14:20:57 +0530

    Update provider.tf

--- envs/dev/05-gitops/provider.tf
+++ envs/dev/05-gitops/provider.tf
@@ -17,3 +17,10 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_ca)
   token                  = data.aws_eks_cluster_auth.this.token
 }
+
+provider "kubectl" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
