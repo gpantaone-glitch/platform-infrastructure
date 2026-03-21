e0f775cb28c65e6f212e549f612000b2f5377744
Gaurav Pantaone <gpantaone@gmail.com>
2026-03-21 14:00:32 +0530

    Update app.tf

--- envs/dev/05-gitops/app.tf
+++ envs/dev/05-gitops/app.tf
@@ -1,33 +1,27 @@
-resource "kubernetes_manifest" "sample_app" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
+resource "kubectl_manifest" "argocd_root_app" {
+  yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: dev-root
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/gpantaone-glitch/platform-workload.git
+    targetRevision: main
+    path: argocd/dev
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: argocd
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+YAML
 
-    metadata = {
-      name      = "tns"
-      namespace = "argocd"
-    }
-
-    spec = {
-      project = "default"
-
-      source = {
-        repoURL        = "https://github.com/gpantaone-glitch/platform-workload.git"
-        targetRevision = "main"
-        path           = "argocd/dev"
-      }
-
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = "argocd"
-      }
-
-      syncPolicy = {
-        automated = {
-          prune    = true
-          selfHeal = true
-        }
-      }
-    }
-  }
+  depends_on = [
+    helm_release.argocd,
+    kubernetes_namespace.tns
+  ]
 }
